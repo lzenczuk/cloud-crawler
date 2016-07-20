22d5ded12224ffb0ab4c87dd6f4da18bc0d9d2e0
Lucjan Zenczuk <lucjan.zenczuk@gmail.com.com>
2016-07-20 18:01:51 +0100

    Fix problem with inheritance in Messages. This way is incorrect because pollute message model with jackson annotation.

--- src/main/java/com/github/lzenczuk/crawler/scenario/impl/poloniex/stream/Message.java
+++ src/main/java/com/github/lzenczuk/crawler/scenario/impl/poloniex/stream/Message.java
@@ -1,7 +1,10 @@
 package com.github.lzenczuk.crawler.scenario.impl.poloniex.stream;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /**
  * Created by dev on 15/07/16.
  */
+@JsonTypeInfo(include = JsonTypeInfo.As.PROPERTY, use = JsonTypeInfo.Id.CLASS, property = "messageType")
 public interface Message {
 }
--- src/main/java/com/github/lzenczuk/crawler/scenario/impl/poloniex/stream/json/JsonMessageMapper.java
+++ src/main/java/com/github/lzenczuk/crawler/scenario/impl/poloniex/stream/json/JsonMessageMapper.java
@@ -2,11 +2,16 @@ package com.github.lzenczuk.crawler.scenario.impl.poloniex.stream.json;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.jsontype.*;
 import com.fasterxml.jackson.databind.jsontype.impl.StdTypeResolverBuilder;
 import com.github.lzenczuk.crawler.scenario.impl.poloniex.stream.Message;
 
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  * Created by dev on 20/07/16.
@@ -17,12 +22,6 @@ public class JsonMessageMapper {
 
     public JsonMessageMapper() {
         objectMapper = new ObjectMapper();
-
-        StdTypeResolverBuilder stdTypeResolverBuilder = new StdTypeResolverBuilder();
-        stdTypeResolverBuilder.init(JsonTypeInfo.Id.CLASS, null);
-        stdTypeResolverBuilder.inclusion(JsonTypeInfo.As.PROPERTY);
-
-        objectMapper.setDefaultTyping(stdTypeResolverBuilder);
     }
 
     public JsonMessageMapper(ObjectMapper objectMapper) {
